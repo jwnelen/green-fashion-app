6102dcf800b186fadcc9e49c56e4c8a7d5d3ee80
Jeroen Nelen <jwnelen@gmail.com>
2025-08-15 13:41:10 +0200

    Final changes

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import { AuthProvider } from './contexts/AuthContext'
+import { AuthProvider } from './contexts/AuthProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
