801716dbc2c41077c03b516f4009785b41df7293
Jeroen Nelen <jwnelen@gmail.com>
2025-08-17 17:14:41 +0200

    Lets goo

--- infrastructure/modules/auth/main.tf
+++ infrastructure/modules/auth/main.tf
@@ -1,16 +1,16 @@
 resource "google_secret_manager_secret" "google_client_id" {
-    secret_id = "${var.service_name}-google-client-id-${var.environment}"
+  secret_id = "${var.service_name}-google-client-id-${var.environment}"
 
-    replication {
-      auto {}
-    }
+  replication {
+    auto {}
   }
+}
 
-  # Google Client Secret (sensitive, must be in Secret Manager)
-  resource "google_secret_manager_secret" "google_client_secret" {
-    secret_id = "${var.service_name}-google-client-secret-${var.environment}"
+# Google Client Secret (sensitive, must be in Secret Manager)
+resource "google_secret_manager_secret" "google_client_secret" {
+  secret_id = "${var.service_name}-google-client-secret-${var.environment}"
 
-    replication {
-      auto {}
-    }
+  replication {
+    auto {}
   }
+}
